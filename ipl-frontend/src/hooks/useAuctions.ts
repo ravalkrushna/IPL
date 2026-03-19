7952641488034b4e23272cd951c9ebfb2f3f77b3
krushnaraval7@gmail.com <krushnaraval7@gmail.com>
2026-03-19 15:30:00 +0530

    removed unused files

--- ipl-frontend/src/hooks/useAuctions.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query"
-import { auctionApi } from "@/lib/auctionApi"
-
-export function useAuctions() {
- return useQuery({
-   queryKey: ["auctions"],
-  queryFn:  auctionApi.list,
- })
-}
