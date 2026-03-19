72f27c3ebb0a06a9d9810c2112aa5fa398fed88d
krushnaraval7@gmail.com <krushnaraval7@gmail.com>
2026-03-19 13:31:25 +0530

    some updates regarding cors in backend

--- ipl_backend/src/main/java/com/example/ipl_backend/IplBackendApplication.java
+++ ipl_backend/src/main/java/com/example/ipl_backend/IplBackendApplication.java
@@ -4,12 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {},
+        excludeName = {"org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration"}
+)
 @EnableScheduling
 public class IplBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(IplBackendApplication.class, args);
     }
-
 }
